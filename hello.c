7556baf72b84e9059584e055660eaee18c3d33f7
IsaiahJohnstone <isaiahjohnatone@gmail.com>
2026-05-13 15:27:26 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
 
-int main(void) {printf("Hello,world!/n");printf{"Just to show the other side.\n");}
+int main(void) {printf("Hello,world!\n");printf{"Just to show the other side.\n");}
 
